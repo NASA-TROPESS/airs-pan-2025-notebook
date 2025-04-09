a955e42bb6104cded702f86e20a01e35e8d9364f
Joshua Laughner <josh.laughner@jpl.nasa.gov>
2025-04-08 17:05:16 -0700

    Updated custom dependencies to use pyproject.toml installs

--- src/muses_utils/setup.py
+++ src/muses_utils/setup.py
@@ -1,13 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='MUSESUtils',
-    version='0.1',
-    packages=find_packages(),
-    #install_requires=['cftime', 'matplotlib', 'netCDF4', 'numpy', 'pandas', 'scipy', 'statsmodels'],
-    #url='https://github.com/joshua-laughner/JLL-Py-Utils',
-    license='',
-    author='Joshua Laughner',
-    author_email='josh.laughner@jpl.nasa.gov',
-    description='Python utilities for working with MUSES data'
-)
+from setuptools import setup
+setup()
